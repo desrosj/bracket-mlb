584340db812233189b0da2d66823575b0f8f9da7
Jonathan Desrosiers <desrosj@users.noreply.github.com>
2020-09-29 14:20:30 -0400

    Bring repo up to date with improvements upstream. Add remainder of 2019 information.

--- src/cli.js
+++ src/cli.js
@@ -4,18 +4,18 @@ const yargs = require('yargs');
 const { computeResults } = require('./actions/compute');
 
 yargs
-  .command(
-    'get-score',
-    'Get the current score',
-    () => {},
-    () => {
-      const results = _.reverse(_.sortBy(computeResults(), 'score'));
+	.command(
+		'get-score',
+		'Get the current score',
+		() => {},
+		() => {
+			const results = _.reverse(_.sortBy(computeResults(), 'score'));
 
-      let i;
-      for (i = 0; i < results.length; i++) {
-        const str = `${i + 1}. ${results[i].name} ${results[i].score}`;
-        console.log(str);
-      }
-    }
-  )
-  .argv
+			let i;
+			for (i = 0; i < results.length; i++) {
+				const str = `${i + 1}. ${results[i].name} ${results[i].score}`;
+				console.log(str);
+			}
+		}
+	)
+	.argv
