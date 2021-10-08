169ce37a066f54ed456f7e59f3f9ea2986839964
Jake Spurlock <whyisjake@gmail.com>
2021-10-07 21:21:29 -0700

    Fix for the score.

--- src/helpers/compute.js
+++ src/helpers/compute.js
@@ -11,10 +11,13 @@ const POINTS_FOR_GAMES = 2;
 const getRoundFromGame = (game) => {
 	const mapping = {
 		1: 1,
-		2: 2,
-		3: 2,
-		4: 3,
-		5: 4
+		2: 1,
+		3: 1,
+		4: 1,
+		5: 2,
+		6: 2,
+		7: 3,
+		8: 4
 	};
 
 	const gameNumber = game.slice(-1);
